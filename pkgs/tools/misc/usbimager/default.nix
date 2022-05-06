9baaa9416dc74d29531cec808dde0139a36dbf60
Stanisław Pitucha <stan.pitucha@envato.com>
2022-05-06 21:40:44 +1000

    usbimager: add darwin support

--- pkgs/tools/misc/usbimager/default.nix
+++ pkgs/tools/misc/usbimager/default.nix
@@ -1,9 +1,11 @@
 { lib, stdenv, fetchFromGitLab, pkg-config
 , withLibui ? true, gtk3
 , withUdisks ? stdenv.isLinux, udisks, glib
-, libX11 }:
+, libX11
+, darwin }:
 
-stdenv.mkDerivation rec {
+let frameworks = darwin.apple_sdk.frameworks;
+in stdenv.mkDerivation rec {
   pname = "usbimager";
   version = "1.0.8";
 
@@ -18,15 +20,25 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = lib.optionals withUdisks [ udisks glib ]
-    ++ lib.optional (!withLibui) libX11
-    ++ lib.optional withLibui gtk3;
     # libui is bundled with the source of usbimager as a compiled static libary
+    ++ lib.optional (!withLibui) libX11
+    ++ lib.optional withLibui gtk3
+    ++ lib.optionals stdenv.isDarwin (with frameworks; [ CoreFoundation IOKit DiskArbitration Foundation Cocoa ]);
 
   postPatch = ''
     sed -i \
       -e 's|install -m 2755 -g disk|install |g' \
       -e 's|-I/usr/include/gio-unix-2.0|-I${glib.dev}/include/gio-unix-2.0|g' \
       -e 's|install -m 2755 -g $(GRP)|install |g' Makefile
+    sed -i -e 's|CC\s*=|CC?=|' bzip2/Makefile
+    sed -i -e 's|CC\s*=|CC?=|' xz/Makefile
+    sed -i \
+      -e 's|CC\s*=|CC?=|' \
+      -e 's|-framework CoreFoundation|-framework CoreFoundation -F${frameworks.CoreFoundation}/Library/Frameworks|' \
+      -e 's|-framework DiskArbitration|-framework DiskArbitration -F${frameworks.DiskArbitration}/Library/Frameworks|' \
+      -e 's|-framework Foundation|-framework Foundation -F${frameworks.Foundation}/Library/Frameworks|' \
+      -e 's|-framework Cocoa|-framework Cocoa -F${frameworks.Cocoa}/Library/Frameworks|' \
+      -e 's|-framework IOKit|-framework IOKit -F${frameworks.IOKit}/Library/Frameworks|'  Makefile
   '';
 
   dontConfigure = true;
@@ -42,6 +54,6 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ vdot0x23 ];
     # windows and darwin could work, but untested
     # feel free add them if you have a machine to test
-    platforms = with platforms; linux;
+    platforms = with platforms; unix;
   };
 }
