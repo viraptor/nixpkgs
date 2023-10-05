cb6a824248a5357542c7f78c5bfcae938316cbd4
Stanisław Pitucha <git@viraptor.info>
2023-10-05 21:13:07 +1100

    clang13

--- pkgs/build-support/libredirect/default.nix
+++ pkgs/build-support/libredirect/default.nix
@@ -3,9 +3,9 @@
 let
   # aarch64-darwin needs a clang that can build arm64e binaries, so make sure a version of LLVM
   # is used that can do that, but prefer the stdenv one if it is new enough.
-  llvmPkgs = if (lib.versionAtLeast (lib.getVersion llvmPackages.clang) "13")
-    then llvmPackages
-    else llvmPackages_13;
+  llvmPkgs = llvmPackages_13; #if (lib.versionAtLeast (lib.getVersion llvmPackages.clang) "13")
+    #then llvmPackages
+    #else llvmPackages_13;
   in
 if stdenv.hostPlatform.isStatic
 then throw ''
